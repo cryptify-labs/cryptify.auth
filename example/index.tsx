f3d973778e3d3b5b14e33409c626bd6f90b602d3
Shlok D. Mange <shlok_mange@protonmail.com>
2022-04-03 22:00:12 +0530

    fixed connect wallet css

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { useEffect } from 'react';
-import { useWeb3, DappProvider, ConnectButton } from '../src/index'
+import { useWeb3, DappProvider, ConnectButton } from '@cryptify/auth'
 
 const Test = () => {
 
